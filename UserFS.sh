d292ce2df1a3c7bd64a7985398891a9b65a91d12
dimitrie-gheorghe <dimitriegh@gmail.com>
2025-12-26 20:23:40 +0200

    Am reusit sa adaug un loop care proceseaza utilizatorii activi si creeaza pentru fiecare un director care contine fisierul procs, in care se listeaza procesele utilizatorului. Tot in acest loop, am adaugat niste comenzi care sterg fisierul lastlogin (Deoarece nu mai e nevoie de acest fisier, din moment ce utilizatorul este activ. Bineinteles, deocamdata scriptul nici nu creeaza fisierul lastlogin, acest lucru va fi facut in alt loop, in commit-urile viitoare). De asemenea, am adaugat cateva comentarii, pentru a face script-ul mai usor de inteles.

--- UserFS.sh
+++ UserFS.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
 if [ ! -d "director_radacina" ]; then
-	mkdir "director_radacina"
+	mkdir "director_radacina" # cream directorul radacina, in caz ca nu exista deja
 fi
+
+cd director_radacina
 
 echo "Scriptul UserFS a pornit. Apasa Ctrl+C pentru a il opri"
 
 while true; do
-	utilizatori_activi=$(who | awk '{print $1}' | sort | uniq)
-	echo "$utilizatori_activi"
+	utilizatori_activi=$(who | awk '{print $1}' | uniq) #uniq inseamna ca eliminam duplicatele
+	
+	for nume in $utilizatori_activi; do
+		if [ ! -d "$nume" ]; then
+			mkdir "$nume" # cream un director pentru fiecare utilizator activ, in caz ca nu exista deja
+		fi
+		
+		cd "$nume"
+		if [ -f "lastlogin" ]; then
+			rm "lastlogin" # daca user-ul e activ, nu mai e nevoie de lastlogin
+		fi
+		procese=$(ps -u "$nume")
+		echo "$procese" > ./procs # listam procesele utilizatorului in fisierul procs
+		cd ..
+	done
+
 	break
+	sleep 30 # asteptam 30 de secunde. deocamdata, nu se ajunge aici, pentru ca avem un break
 done
